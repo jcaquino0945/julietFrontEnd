08f4a23a49c212cfe55287634f1ae9c27f715416
Miguel Aquino <jcaquino0945@gmail.com>
2021-01-31 14:41:43 +0800

    Added navbar, routing and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `Home`
+  { path: '**', component: PagenotfoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HomeComponent} from './home/home.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
