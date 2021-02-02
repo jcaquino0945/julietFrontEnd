02436069d3762719b8f7c13666c1c7397ba24104
louis <louisse.evale.iics@ust.edu.ph>
2021-02-02 21:31:57 +0800

    about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import {HomeComponent} from './home/home.component';
-import {PagenotfoundComponent} from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuardService as AuthGuard } from './auth-guard.service';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent, canActivate : [AuthGuard] },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `Home`
