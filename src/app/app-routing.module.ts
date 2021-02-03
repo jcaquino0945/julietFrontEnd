a17655f917e462e1388f64ef80c6f4d864257665
louis <louisse.evale.iics@ust.edu.ph>
2021-02-03 19:23:22 +0800

    4 pages added + fixed navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,16 @@ import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { AuthGuardService as AuthGuard } from './auth-guard.service';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'cart', component: CartComponent },
   { path: 'login', component: LoginComponent },
   { path: 'admin', component: AdminComponent, canActivate : [AuthGuard] },
   { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `Home`
