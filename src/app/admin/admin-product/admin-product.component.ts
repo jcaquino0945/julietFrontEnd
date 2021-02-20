e0d4f35eb3bbbafade66f1da8fd51a1dd49d6d53
louis <louisse.evale.iics@ust.edu.ph>
2021-02-20 23:18:44 +0800

    new product modal

--- src/app/admin/admin-product/admin-product.component.ts
+++ src/app/admin/admin-product/admin-product.component.ts
@@ -1,23 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {ProductService} from '../../services/product.service';
-import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import {
+  FormControl,
+  FormGroupDirective,
+  FormBuilder,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Product } from '../../models/product';
 import { Observable } from 'rxjs';
 import { interval } from 'rxjs';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
 @Component({
   selector: 'app-admin-product',
   templateUrl: './admin-product.component.html',
-  styleUrls: ['./admin-product.component.css']
+  styleUrls: ['./admin-product.component.css'],
 })
 export class AdminProductComponent implements OnInit {
   products$: Product[];
@@ -33,47 +47,64 @@ export class AdminProductComponent implements OnInit {
   //orders
   isLoadingResults = false;
   matcher = new MyErrorStateMatcher();
-  constructor(private router : Router,private productService:ProductService,private formBuilder:FormBuilder) { }
+  constructor(
+    private router: Router,
+    private productService: ProductService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    this.productService.getProducts().subscribe(products$ => this.products$ = products$,
-      errmess => this.errMess = <any>errmess);
-    
-    this.galleryForm = this.formBuilder.group({
-      imageFile : [null, Validators.required],
-      name : [null, Validators.required],
-      description : [null, Validators.required],
-      price : [null, Validators.required],
-      category : [null, Validators.required],
-      stock_quantity : 0,
-      featured: false
+    this.productService.getProducts().subscribe(
+      (products$) => (this.products$ = products$),
+      (errmess) => (this.errMess = <any>errmess)
+    );
 
+    this.galleryForm = this.formBuilder.group({
+      imageFile: [null, Validators.required],
+      name: [null, Validators.required],
+      description: [null, Validators.required],
+      price: [null, Validators.required],
+      category: [null, Validators.required],
+      stock_quantity: 0,
+      featured: false,
     });
   }
   onFormSubmit(): void {
     this.isLoadingResults = true;
-    this.productService.addGallery(this.galleryForm.value, this.galleryForm.get('imageFile').value._files[0])
-      .subscribe((res: any) => {
-        this.isLoadingResults = false;
-        if (res.body) {
-          document.getElementById('close').click();// close modal
-          this.router.navigate(['/admin/adminProduct/', res.body._id]); //navigate to product detail page
-          this.productService.getProducts().subscribe(products$ => this.products$ = products$,
-            errmess => this.errMess = <any>errmess);
+    this.productService
+      .addGallery(
+        this.galleryForm.value,
+        this.galleryForm.get('imageFile').value._files[0]
+      )
+      .subscribe(
+        (res: any) => {
+          this.isLoadingResults = false;
+          if (res.body) {
+            document.getElementById('close').click(); // close modal
+            this.router.navigate(['/admin/adminProduct/', res.body._id]); //navigate to product detail page
+            this.productService.getProducts().subscribe(
+              (products$) => (this.products$ = products$),
+              (errmess) => (this.errMess = <any>errmess)
+            );
+          }
+        },
+        (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
         }
-      }, (err: any) => {
-        console.log(err);
-        this.isLoadingResults = false;
-      });
-      this.galleryForm.reset();
+      );
+    this.galleryForm.reset();
   }
 
   deleteProduct(id) {
-    this.productService.deleteProduct(id).subscribe(products$ => this.products$ = products$,
-      errmess => this.errMess = <any>errmess)
-    
-      this.productService.getProducts().subscribe(products$ => this.products$ = products$,
-        errmess => this.errMess = <any>errmess);
-  }
+    this.productService.deleteProduct(id).subscribe(
+      (products$) => (this.products$ = products$),
+      (errmess) => (this.errMess = <any>errmess)
+    );
 
+    this.productService.getProducts().subscribe(
+      (products$) => (this.products$ = products$),
+      (errmess) => (this.errMess = <any>errmess)
+    );
+  }
 }
