665ef9a1cb84923124a846cae2fd8986fe9501c5
Miguel Aquino <jcaquino0945@gmail.com>
2021-02-20 22:39:21 +0800

    fixed shop product layout

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarService } from '../services/navbar.service';
 import { RibbonService } from '../services/ribbon.service';
 import { FooterService } from '../services/footer.service';
+import {ProductService} from './../services/product.service';
+import { Product } from './../models/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
@@ -9,15 +12,22 @@ import { FooterService } from '../services/footer.service';
   styleUrls: ['./shop.component.css'],
 })
 export class ShopComponent implements OnInit {
+  products$: Product[];
+  errMess: string;
   constructor(
     public nav: NavbarService,
     public ribbon: RibbonService,
-    public footer: FooterService
+    public footer: FooterService,
+    private router : Router,
+    private productService:ProductService
   ) {}
 
   ngOnInit(): void {
     this.nav.show();
     this.ribbon.show();
     this.footer.show();
+    
+    this.productService.getProducts().subscribe(products$ => this.products$ = products$,
+      errmess => this.errMess = <any>errmess);
   }
 }
