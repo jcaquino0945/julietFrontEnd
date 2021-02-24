e4de9eb036536a91c4991ea06602122daf73b0d7
Miguel Aquino <jcaquino0945@gmail.com>
2021-02-24 17:18:52 +0800

    added items from cart to checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -3,17 +3,21 @@ import { NavbarService } from '../services/navbar.service';
 import { RibbonService } from '../services/ribbon.service';
 import { FooterService } from '../services/footer.service';
 import Stepper from 'bs-stepper';
-
+import { CartService } from '../services/cart.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  items = this.cartService.getItems();
+  totalPrice = this.cartService.totalPrice();
+  itemPrice = this.cartService.itemPrice();
   constructor(
     public nav: NavbarService,
     public ribbon: RibbonService,
-    public footer: FooterService
+    public footer: FooterService,
+    private cartService: CartService
   ) {}
 
   title = 'stepper';
