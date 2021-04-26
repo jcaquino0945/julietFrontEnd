8a53bf27fd2de4f41346f6ad7527a036fbd3d8d1
hanspatricknituda <hanspatrick.nituda.iics@ust.edu.ph>
2021-04-26 23:40:50 +0800

    necessary updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -54,3 +54,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { NavbarService } from '../services/navbar.service';
 import { ResponsiveService } from '../services/responsive.service';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -24,5 +26,14 @@ export class NavbarComponent implements OnInit {
     this.responsive.getMobileStatus().subscribe((isMobile) => {
       this.isMobile = isMobile;
     });
+  }
+}
+export class AppComponent  {
+  constructor(
+    router: Router
+  ) {
+    router.events
+          .pipe(filter((routerEvent: Event) => routerEvent instanceof NavigationEnd))
+          .subscribe(() => window.scrollTo(0, 0));
   }
 }
