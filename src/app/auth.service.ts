f72b19ae37d5bd4862d5cc6f95948a8d9af6f093
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-30 15:36:35 +0800

    deploy

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
 
   public validate(username, password) {
     return this.http
-      .post('http://localhost:3000/auth/login', {
+      .post('/auth/login', {
         username: username,
         password: password,
       })
